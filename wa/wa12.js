759286f93da65c43090f8524217b45bc3b5d64a5
provalisk <trph3191@colorado.edu>
2022-11-08 10:39:34 -0700

    idk

--- wa/wa12.js
+++ wa/wa12.js
@@ -1,4 +1,49 @@
-const button = document.querySelector('.button');
+function check() {
+    console.log('test');
+}
+
+function submit() {
+    alert(output.textContent);
+}
+
+function reset() {
+    outputInt = 0;
+    output.textContent = outputInt;
+}
+
+function minus() {
+    if (outputInt > 0) {
+    outputInt -=1
+    output.textContent = outputInt; }
+}
+
+function plus() {
+    if(outputInt > 9999999999) {
+    outputInt +=1
+    output.textContent = outputInt; }
+}
+
+function random() {
+    outputInt = randomNumber(0, 9999999999);
+    output.textContent = outputInt;
+}
+
+function randomNumber(min, max) {
+    const num = Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+const output = document.querySelector('.output');
+const outputInt =  parseInt(output.textContent);
+console.log(outputInt);
+
+const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+
+
+/*const button = document.querySelector('.button');
+
 const output = document.querySelector('.output');
 let phone_content = document.querySelector('.phone');
 
@@ -6,4 +51,6 @@ button.addEventListener('click', updateOutput);
 
 function updateOutput() {
     output.textContent = phone_content.value;
+    alert(phone_content.value);
 }
+*/
